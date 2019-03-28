c0f8d3900a23a013fbd60d72fdf9519630e58189
Blake Abel <blakecabel@gmail.com>
2019-03-28 19:41:55 -0400

    Starting back up development on my website

--- public/app.js
+++ public/app.js
@@ -1,2 +1,15 @@
 import {MDCRipple} from '@material/ripple';
 const ripple = new MDCRipple(document.querySelector('.source-button'));
+
+ $(document).ready(function(){
+     $.ajax({
+         url: 'https://gitlab.com/api/v4/users/blabel3/projects',
+         type:"GET",
+         success: function(result){
+             console.log(result)
+         },
+         error: function(error){
+             console.log('Error ${error}')
+         }
+     })
+ })
